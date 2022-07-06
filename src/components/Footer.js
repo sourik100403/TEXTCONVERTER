a7f1634fb6ae595691fa00af3ecf55477fd6eb10
sourik100403 <sourikparui100403@gmail.com>
2022-07-06 23:23:17 +0530

    hfhgjnm,mnkjb

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    
+    <div>
+      <footer className="bg-light text-center text-lg-start">
+         <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:<b>Sourik Parui</b>
+         <a className="text-dark">MyTextConverter.com</a>
+       </div>
+</footer>
+    </div>
+  )
+}
